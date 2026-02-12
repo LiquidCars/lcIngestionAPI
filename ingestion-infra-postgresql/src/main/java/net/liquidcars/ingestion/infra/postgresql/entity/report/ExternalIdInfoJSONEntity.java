7f4b7be0638a58d104878ecb81fbe27a963a772b
alexb <alejandro@liquidcars.net>
2026-02-12 15:29:27 +0100

    https://liquidcars.atlassian.net/browse/LIQUID-263

--- ingestion-infra-postgresql/src/main/java/net/liquidcars/ingestion/infra/postgresql/entity/report/ExternalIdInfoJSONEntity.java
+++ ingestion-infra-postgresql/src/main/java/net/liquidcars/ingestion/infra/postgresql/entity/report/ExternalIdInfoJSONEntity.java
@@ -1,13 +1,11 @@
 package net.liquidcars.ingestion.infra.postgresql.entity.report;
 
-import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
